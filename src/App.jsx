0d3ae421a5177e5cb6bd900c49b934595be0bef0
debasish-itobuz <debasish.das@itobuz.com>
2024-07-30 12:14:52 +0530

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,34 @@
 import Player from "./components/Player";
+import GameBoard from "./components/GameBoard";
+// import Log from "./components/Log";
+import { useState } from "react";
 
 function App() {
+  const [activePlayer, setActivePlayer] = useState("X");
+  // cont[gameTurns, setGameTurns]= useState([])
+
+  function handleselectSquare() {
+    setActivePlayer((cuurentPlayer) => (cuurentPlayer === "X" ? "O" : "X"));
+  }
   return (
     <>
       <h1>Tic-Tac-Toe</h1>;
       <div id="game-container">
-        <ol id="players">
-          <Player initialName="Player1" symbol="X" />
-          <Player initialName="Player2" symbol="O" />
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName="Player1"
+            symbol="X"
+            isActive={activePlayer === "X"}
+          />
+          <Player
+            initialName="Player2"
+            symbol="O"
+            isActive={activePlayer === "O"}
+          />
         </ol>
-        Game Board
+        <GameBoard onselectSquare={handleselectSquare}  activePlayerSymbol={activePlayer}/>
       </div>
+      {/* <Log/> */}
     </>
   );
 }
